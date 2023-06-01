bb6b66db7223177cd8e1626cdcd94c5d8dc2ffea
dzeleniak <zeleniak.david@gmail.com>
2023-05-31 18:17:33 -0700

    Add Google Authentication

--- client/app/components/Modals/RegisterModal.tsx
+++ client/app/components/Modals/RegisterModal.tsx
@@ -68,7 +68,7 @@ const RegisterModal = () => {
                 outline
                 label="Continue with Google"
                 icon={FcGoogle}
-                onClick={()=>{}}
+                onClick={()=>signIn('google')}
             />
             <Button 
                 outline
